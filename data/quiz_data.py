40c7dc3f8ea0480a55a7ac04c1a56ef7f9d0be43
dayeonlee <dl2950@columbia.edu>
2022-04-15 03:09:14 -0400

    Add mcq quiz template and data

--- data/quiz_data.py
+++ data/quiz_data.py
@@ -1,3 +1,34 @@
 # Quiz data in the format of 
 # {page_num: { contents for quiz including question / answer / relevant tutorial page .. }}
-quiz_data = {"1": {"type": "mcq"}}
+quiz_data = {
+    "1": {"type": "image_mcq"},
+    "3": {
+        "id": "3",
+        "type": "mcq",
+        "previous": "2",
+        "next": "4",
+        "learn": "10",
+        "question": "Select the correct option:",
+        "answer": "3",
+        "choices":{
+            "1": "The darkest shade of wine is the best.",
+            "2": "The more expensive the wine, the better it is.",
+            "3": "Color is a good indicator of the age of a wine."
+        }
+    },
+    "7": {
+        "id": "7",
+        "type": "mcq",
+        "previous": "6",
+        "next": "none",
+        "learn": "13",
+        "question": "Which one of the following wine is the most popular red wine?",
+        "answer": "1",
+        "choices":{
+            "1": "Cabernet Sauvignon",
+            "2": "Pinot Noir",
+            "3": "Sauvignon Blanc",
+            "4": "Zinfandel"
+        }
+    },
+}
---
+++ static/quiz_mcq.js
@@ -0,0 +1,22 @@
+function initialize_choices(choices) {
+    $("#mcq_choices").empty()
+    $.each(Object.keys(choices), function(index, choice_idx){
+        let div = $("<div class='mcq_choice'></div>")
+        let choice = choices[choice_idx]
+        console.log(choice)
+        let button = $("<input type='radio' name='choice'></div>")
+        button.prop({
+            id: choice_idx,
+            value: choice_idx
+        })        
+        let label = $("<label for='${choice_idx}' class='mcq_choice_label'></label><br>'")
+        label.html(choice)
+
+        div.append(button).append(label)
+        $("#mcq_choices").append(div)
+    })
+}
+
+$(document).ready(function(){
+    initialize_choices(item["choices"])
+})
