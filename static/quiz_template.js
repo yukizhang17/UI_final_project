9fde0e8115ae9895b1c0eca22ed69c80432ea289
dayeonlee <dl2950@columbia.edu>
2022-04-16 18:53:30 -0400

    Add buttons to quiz template

---
+++ static/quiz_template.js
@@ -0,0 +1,37 @@
+function onSubmit() {
+    // Add Previous / Next / Learn buttons once submit is clicked.
+    // This function should be called in submit() function for individual question types
+    showActionButtons()
+    setClickActions()
+}
+
+function setClickActions() {
+    // TODO: Set click actions for action buttons (prev / next / learn)
+}
+
+function showActionButtons() {
+    $("#quizactions").empty()
+    if (item['previous'] != 'none') {
+        let prevbutton = $("<div class='custom-btn btn-type-2 mx-2' id='previous'></div>").html("Previous")
+        $("#quizactions").append(prevbutton)
+    }
+    if (item['next'] != 'none') {
+        let nextbutton = $("<div class='custom-btn btn-type-2 mx-2' id='next'></div>").html("Next")
+        $("#quizactions").append(nextbutton)
+    }
+
+    let learnbutton = $("<div class='custom-btn btn-type-2 mx-2' id='learn'></div>").html("Learn")
+    $("#quizactions").append(learnbutton)
+}
+
+function showSubmitButton() {
+    $("#quizactions").empty()
+    
+    let submitbutton = $("<div class='custom-btn btn-type-2 mx-2' id='submit'></div>")
+    submitbutton.html("Submit")
+    $("#quizactions").append(submitbutton)
+}
+
+$(document).ready(function(){
+    showSubmitButton()
+})
